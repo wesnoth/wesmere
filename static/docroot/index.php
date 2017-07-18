0d0e3373b359fb9033159d85895184ffcf1d6a85
Ignacio R. Morelle <shadowm@wesnoth.org>
2017-07-17 21:59:01 -0400

    Wesmere 1.0.0 RC 2

--- static/docroot/index.php
+++ static/docroot/index.php
@@ -28,7 +28,7 @@ if (php_sapi_name() !== "cli")
 }
 
 $show_build = gethostname() === "hanacore";
-$build_version = "1.0.0-RC1";
+$build_version = "1.0.0-RC2";
 
 $use_css_versioning = true;
 $css_version = "1.0.0";
