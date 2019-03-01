7dba822b91ac101550d610b36cb83c4a4b2118f6
Charles Dang <exodia339@gmail.com>
2019-03-02 07:53:30 +1100

    Fixup a03594b

--- static/docroot/index.php
+++ static/docroot/index.php
@@ -82,7 +82,7 @@ $branches = [
 
 		'release-notes' => [ 'url' => '/start/1.14/', 'label' => 'Release notes for 1.14' ],
 
-		'files-url-prefix' => 'https://sourceforge.net/projects/wesnoth/files/wesnoth-1.14/wesnoth-1.14.5',
+		'files-url-prefix' => 'https://sourceforge.net/projects/wesnoth/files/wesnoth-1.14/wesnoth-1.14.6',
 
 		'files' =>
 		[
