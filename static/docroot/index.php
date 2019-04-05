22bdc2b52fdadaf675150dbd9544036e8f81eed4
Iris Morelle <shadowm@wesnoth.org>
2019-04-05 01:01:42 -0300

    Wesmere 1.1.7

--- static/docroot/index.php
+++ static/docroot/index.php
@@ -28,7 +28,7 @@ if (php_sapi_name() !== "cli")
 }
 
 $show_build = gethostname() === "hanacore";
-$build_version = "1.1.7-dev";
+$build_version = "1.1.7";
 
 $use_css_versioning = true;
 $css_version = "1.1.7";
