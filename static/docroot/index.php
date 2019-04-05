5438e21055d67fc2490a2395e9f054d7d04c9214
Iris Morelle <shadowm@wesnoth.org>
2019-04-05 00:49:58 -0300

    Bump version

--- static/docroot/index.php
+++ static/docroot/index.php
@@ -28,7 +28,7 @@ if (php_sapi_name() !== "cli")
 }
 
 $show_build = gethostname() === "hanacore";
-$build_version = "1.1.6";
+$build_version = "1.1.7-dev";
 
 $use_css_versioning = true;
 $css_version = "1.1.6";
