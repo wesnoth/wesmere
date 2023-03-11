5f905f0004083cbd2b93683f3364796e9ee1a8d9
Pentarctagon <pentarctagon@tutamail.com>
2023-03-11 10:48:32 -0600

    Update home page for macos now being up to date.

--- static/docroot/index.php
+++ static/docroot/index.php
@@ -98,7 +98,7 @@ $branches = [
 			[
 				'os'    => 'apple',
 				'label' => 'macOS (10.12+)',
-				'url'   => 'https://sourceforge.net/projects/wesnoth/files/wesnoth-1.16/wesnoth-1.16.6/Wesnoth_1.16.6.dmg/download',
+				'url'   => 'https://sourceforge.net/projects/wesnoth/files/wesnoth-1.16/wesnoth-1.16.8/Wesnoth_1.16.8.dmg/download',
 			],
 			[
 				'os'    => 'linux',
@@ -167,7 +167,7 @@ $branches = [
 			[
 				'os'    => 'apple',
 				'label' => 'macOS (10.12+)',
-				'url'   => 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.17.10/Wesnoth_1.17.10.dmg/download',
+				'url'   => 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.17.13/Wesnoth_1.17.13.dmg/download',
 			],
 			[
 				'os'    => 'linux',
