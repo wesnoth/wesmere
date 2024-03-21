feca8dcff42a0263e8b1c962d8a217da124eedc3
Iris Morelle <shadowm@wesnoth.org>
2024-03-21 18:49:22 -0300

    mw: Bump CSS version harder

--- wesmere/mw/SkinWesmere.php
+++ wesmere/mw/SkinWesmere.php
@@ -12,7 +12,7 @@ class SkinWesmere extends SkinTemplate
 	public $stylename = 'Wesmere';
 	public $template = 'WesmereTemplate';
 	public $wesmerePrefix = '';
-	public $wesmereCssVersion = '1.1.11';
+	public $wesmereCssVersion = '1.2.0';
 
 	public function __construct()
 	{
