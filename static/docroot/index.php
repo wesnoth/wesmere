d0a66d652968eb97c3258f4df4f57542a6626911
pentarctagon <pentarctagon@tutamail.com>
2024-11-08 20:57:57 -0600

    home: Update for Wesnoth 1.18.3.

--- static/docroot/index.php
+++ static/docroot/index.php
@@ -102,7 +102,7 @@ $branches = [
 	'stable' =>
 	[
 		'label'       => 'Stable',
-		'version'     => '1.18.2',
+		'version'     => '1.18.3',
 		'url'         => 'https://wiki.wesnoth.org/Download#Stable_.281.18_branch.29',
 		'recommended' => true,
 
@@ -110,11 +110,11 @@ $branches = [
 			'<p>The <b>stable</b> version of Wesnoth is recommended for new and veteran players and content creators on all platforms, as it offers a well-supported and extensively-tested experience, with new releases delivering bug fixes and translation updates.</p>' .
 			'<p>Players can also obtain this version of Wesnoth from <a href="' . htmlspecialchars($config['steam-store-link']) . '">Steam</a> and the <a href="' . htmlspecialchars($config['mac-store-link']) . '">Mac App Store</a>, with the added benefit of continuous automatic updates.',
 
-		'update-announcement' => 'https://forums.wesnoth.org/viewtopic.php?t=58540',
+		'update-announcement' => 'https://forums.wesnoth.org/viewtopic.php?t=58849',
 
 		'release-notes' => [ 'url' => '/start/1.18/', 'label' => 'Release notes for 1.18' ],
 
-		'files-url-prefix' => 'https://sourceforge.net/projects/wesnoth/files/wesnoth-1.18/wesnoth-1.18.2',
+		'files-url-prefix' => 'https://sourceforge.net/projects/wesnoth/files/wesnoth-1.18/wesnoth-1.18.3',
 
 		'files' =>
 		[
@@ -134,7 +134,7 @@ $branches = [
 			],
 			[
 				'os'    => 'src',
-				'url'   => '@/wesnoth-1.18.2.tar.bz2/download',
+				'url'   => '@/wesnoth-1.18.3.tar.bz2/download',
 			],
 			/*[
 				'os'    => 'ios',
