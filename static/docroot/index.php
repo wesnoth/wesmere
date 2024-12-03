bb98227a9dbfcda0f82497e28bae2d3ef514c36c
pentarctagon <pentarctagon@tutamail.com>
2024-12-02 19:21:01 -0600

    home: Update for Wesnoth 1.19.6.

--- static/docroot/index.php
+++ static/docroot/index.php
@@ -185,16 +185,16 @@ $branches = [
 	[
 		'hidden'      => false,
 		'label'       => 'Development',
-		'version'     => '1.19.5',
+		'version'     => '1.19.6',
 		'url'         => 'https://wiki.wesnoth.org/Download#Development_.281.19_branch.29',
 
 		'description' =>
 			'<p>The <b>development</b> version of Wesnoth is geared towards veteran players and content creators who wish to try out the latest additions to the game. Updates are not guaranteed to be stable and may include game-breaking changes.</p>' .
 			'<p>Players can also obtain this version of Wesnoth from <a href="' . htmlspecialchars($config['steam-store-link']) . '">Steam</a> by selecting it in the Betas tab in the game’s properties after installation, with the added benefit of continuous automatic updates.',
 
-		'update-announcement' => 'https://forums.wesnoth.org/viewtopic.php?t=58799',
+		'update-announcement' => 'https://forums.wesnoth.org/viewtopic.php?t=58935',
 
-		'files-url-prefix' => 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.19.5',
+		'files-url-prefix' => 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.19.6',
 
 		'files' =>
 		[
@@ -214,7 +214,7 @@ $branches = [
 			],
 			[
 				'os'    => 'src',
-				'url'   => '@/wesnoth-1.19.5.tar.bz2/download',
+				'url'   => '@/wesnoth-1.19.6.tar.bz2/download',
 			],
 		],
 
